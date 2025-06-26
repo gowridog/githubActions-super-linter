a24b3944a658bb61396d7323d821187d1f40ec90
srinivas <93887415+gowridog@users.noreply.github.com>
2025-06-26 12:44:06 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,13 @@
 package com.example;
 
-// Calculator class
-
-
 public class Calculator {
-
     /** Adds two integers. */
-    public int add(int a, int b) {    // add method
+    public int add(int a, int b) {
         return a + b;
     }
 
     /** Subtracts b from a. */
-    public int subtract(int a, int b) {    // subtract method
+    public int subtract(int a, int b) {
         return a - b;
     }
 }
