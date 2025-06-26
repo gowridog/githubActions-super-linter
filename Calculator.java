a44fc077a0b9581f15acc0db3c78f810d97a6e4a
srinivas <93887415+gowridog@users.noreply.github.com>
2025-06-26 12:58:28 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,28 @@
 package com.example;
 
+/**
+ * Calculator class for basic operations.
+ */
 public class Calculator {
-    /** Adds two integers. */
+
+    /**
+     * Adds two integers.
+     * 
+     * @param a First number
+     * @param b Second number
+     * @return Sum of a and b
+     */
     public int add(int a, int b) {
         return a + b;
     }
 
-    /** Subtracts b from a. */
+    /**
+     * Subtracts second integer from first.
+     * 
+     * @param a First number
+     * @param b Second number
+     * @return Result of a - b
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
